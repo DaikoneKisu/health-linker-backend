817a57715f7c3b8499fcfc5ce32ad739f08633b0
DaikoneKisu <eduardoarzolay@gmail.com>
2024-06-14 15:00:17 -0400

    feat(manage admins): expose admin routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { App } from '@/app'
 import { CommonController } from '@/controllers/common.controller'
 import { UserController } from '@/controllers/user.controller'
-import { AuthController } from './controllers/auth.controllers'
+import { AuthController } from '@/controllers/auth.controllers'
+import { AdminController } from '@/controllers/admin.controller'
 
-const app = new App([CommonController, UserController, AuthController])
+const app = new App([CommonController, UserController, AuthController, AdminController])
 
 app.listen()
