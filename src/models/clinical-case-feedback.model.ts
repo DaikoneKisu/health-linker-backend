9bde6bf0a43dd3a141f61dd013e91d260f4fe003
DaikoneKisu <eduardoarzolay@gmail.com>
2024-06-20 12:12:48 -0400

    feat(manage clinical cases feedbacks): add clinical cases feedbacks types, model and repository

---
+++ src/models/clinical-case-feedback.model.ts
@@ -0,0 +1,31 @@
+import { integer, pgTable, timestamp, varchar, primaryKey, text } from 'drizzle-orm/pg-core'
+import { clinicalCaseModel } from './clinical-case.model'
+import { userModel } from './user.model'
+
+export const clinicalCaseFeedbackModel = pgTable(
+  'clinical_case_feedback',
+  {
+    id: integer('id').notNull(),
+    clinicalCaseId: integer('clinical_case_id')
+      .notNull()
+      .references(() => clinicalCaseModel.id, {
+        onUpdate: 'cascade',
+        onDelete: 'restrict'
+      }),
+    userDocument: varchar('user_document', { length: 10 })
+      .notNull()
+      .references(() => userModel.document, {
+        onDelete: 'restrict',
+        onUpdate: 'cascade'
+      }),
+    text: text('text').notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true, precision: 6 }).notNull().defaultNow(),
+    updatedAt: timestamp('updated_at', { withTimezone: true, precision: 6 })
+      .notNull()
+      .$onUpdate(() => new Date())
+      .defaultNow()
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.id, table.clinicalCaseId, table.userDocument] })
+  })
+)
---
+++ src/repositories/clinical-case-feedback.repository.ts
@@ -0,0 +1,176 @@
+import { eq, and } from 'drizzle-orm'
+import { clinicalCaseFeedbackModel } from '@/models/clinical-case-feedback.model'
+import { pgDatabase } from '@/pg-database'
+import {
+  FindClinicalCaseFeedback,
+  NewClinicalCaseFeedback,
+  UpdateClinicalCaseFeedback
+} from '@/types/clinical-case-feedback.type'
+import { PgDatabase } from '@/types/pg-database.type'
+
+export class ClinicalCaseFeedbackRepository {
+  private readonly _db: PgDatabase = pgDatabase
+
+  public async findAll(): Promise<FindClinicalCaseFeedback[]> {
+    return await this._db
+      .select({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+      .from(clinicalCaseFeedbackModel)
+  }
+
+  public async findWithLimitAndOffset(
+    limit: number,
+    offset: number
+  ): Promise<FindClinicalCaseFeedback[]> {
+    const rows = await this._db
+      .select({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+      .from(clinicalCaseFeedbackModel)
+      .limit(limit)
+      .offset(limit * offset)
+
+    return rows
+  }
+
+  public async find(
+    id: number,
+    clinicalCaseId: number,
+    userDocument: string
+  ): Promise<FindClinicalCaseFeedback | undefined> {
+    const rows = await this._db
+      .select({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+      .from(clinicalCaseFeedbackModel)
+      .where(
+        and(
+          eq(clinicalCaseFeedbackModel.id, id),
+          eq(clinicalCaseFeedbackModel.clinicalCaseId, clinicalCaseId),
+          eq(clinicalCaseFeedbackModel.userDocument, userDocument)
+        )
+      )
+
+    return rows.at(0)
+  }
+
+  public async findByClinicalCase(clinicalCaseId: number): Promise<FindClinicalCaseFeedback[]> {
+    const rows = await this._db
+      .select({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+      .from(clinicalCaseFeedbackModel)
+      .where(eq(clinicalCaseFeedbackModel.clinicalCaseId, clinicalCaseId))
+
+    return rows
+  }
+
+  public async findByUser(userDocument: string): Promise<FindClinicalCaseFeedback[]> {
+    const rows = await this._db
+      .select({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+      .from(clinicalCaseFeedbackModel)
+      .where(eq(clinicalCaseFeedbackModel.userDocument, userDocument))
+
+    return rows
+  }
+
+  public async create(
+    newClinicalCaseFeedback: NewClinicalCaseFeedback
+  ): Promise<FindClinicalCaseFeedback | undefined> {
+    const rows = await this._db
+      .insert(clinicalCaseFeedbackModel)
+      .values(newClinicalCaseFeedback)
+      .returning({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+
+    return rows.at(0)
+  }
+
+  public async update(
+    id: number,
+    clinicalCaseId: number,
+    userDocument: string,
+    updateClinicalCaseFeedback: UpdateClinicalCaseFeedback
+  ): Promise<FindClinicalCaseFeedback | undefined> {
+    const rows = await this._db
+      .update(clinicalCaseFeedbackModel)
+      .set(updateClinicalCaseFeedback)
+      .where(
+        and(
+          eq(clinicalCaseFeedbackModel.id, id),
+          eq(clinicalCaseFeedbackModel.clinicalCaseId, clinicalCaseId),
+          eq(clinicalCaseFeedbackModel.userDocument, userDocument)
+        )
+      )
+      .returning({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+
+    return rows.at(0)
+  }
+
+  public async delete(
+    id: number,
+    clinicalCaseId: number,
+    userDocument: string
+  ): Promise<FindClinicalCaseFeedback | undefined> {
+    const rows = await this._db
+      .delete(clinicalCaseFeedbackModel)
+      .where(
+        and(
+          eq(clinicalCaseFeedbackModel.id, id),
+          eq(clinicalCaseFeedbackModel.clinicalCaseId, clinicalCaseId),
+          eq(clinicalCaseFeedbackModel.userDocument, userDocument)
+        )
+      )
+      .returning({
+        id: clinicalCaseFeedbackModel.id,
+        clinicalCaseId: clinicalCaseFeedbackModel.clinicalCaseId,
+        userDocument: clinicalCaseFeedbackModel.userDocument,
+        text: clinicalCaseFeedbackModel.text,
+        createdAt: clinicalCaseFeedbackModel.createdAt,
+        updatedAt: clinicalCaseFeedbackModel.updatedAt
+      })
+
+    return rows.at(0)
+  }
+}
